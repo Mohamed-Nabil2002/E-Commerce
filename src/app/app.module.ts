7bfbf526a0069b1afd15009d40b26b0da4128cf5
Mohamed-Nabil2002 <96075224+Mohamed-Nabil2002@users.noreply.github.com>
2024-04-06 16:34:43 +0200

    implement products page and edit copyright style in navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
